c97706f4ae41d39c760167797e8d5d6f13a67120
= <saptadeepbhowmik7@gmail.com>
2020-09-23 16:29:20 +0530

    Add Git Screenshot Readme file

---
+++ README-GIT.md
@@ -0,0 +1,12 @@
+# Git Initial Setup
+![image](https://user-images.githubusercontent.com/12189997/93873263-0df89680-fcef-11ea-9dc0-ee1ea25220c7.png)
+
+# Git Commands
+![image](https://user-images.githubusercontent.com/12189997/93873304-2072d000-fcef-11ea-9c44-2fbb6f7ebdf2.png)
+![image](https://user-images.githubusercontent.com/12189997/93873348-34b6cd00-fcef-11ea-9646-717253a475a3.png)
+
+# Git Branch
+![image](https://user-images.githubusercontent.com/12189997/93873407-49936080-fcef-11ea-83ad-d2e153f3730e.png)
+
+# Advance git commands
+![image](https://user-images.githubusercontent.com/12189997/93873455-5b750380-fcef-11ea-8219-2f1c7e29bf97.png)
---
+++ src/main/java/com/CalculatorService.java
@@ -0,0 +1,39 @@
+package com;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorService {
+	int a = 60, b = 15;
+
+	@GetMapping("/add")
+	public int add() {
+		return a + b;
+	}
+
+	@GetMapping("/subtract")
+	public int subtract() {
+		return a - b;
+	}
+
+	@GetMapping("/multiple")
+	public int multiple() {
+		return a * b;
+	}
+
+	@GetMapping("/division")
+	public int division() {
+		if (b != 0) {
+			return a + b;
+		} else {
+			return 0;
+		}
+	}
+
+	@GetMapping("/modularDivision")
+	public int modularDivision() {
+		return a % b;
+	}
+
+}
